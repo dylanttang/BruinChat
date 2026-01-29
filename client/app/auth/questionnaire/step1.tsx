a19507eb3dacb2f909855431c78080375d241375
michellelee6 <michelleklee07@gmail.com>
2026-01-28 20:27:58 -0800

    Navigation structure and folder organization

---
+++ client/app/auth/questionnaire/step1.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Step1() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Questionnaire – Step 1</Text>
+    </View>
+  );
+}
